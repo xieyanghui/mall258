07edafa86f7245ffc4fbfe1f3d5083a9cacb4f51
xiehui <xieiyanghui@126.com>
2016-04-25 13:33:53 +0800

    4.14

--- smartyTest.php
+++ smartyTest.php
@@ -9,5 +9,13 @@ foreach ($gp as $g){
     $gp->set('gpi_img',str_replace('_430x430q90.jpg','',$g['gpi_img']));
 }
 $gp->save();
+$gp = new Goods();
+$gp->query(new Where(),"g_id,g_img");
+//$gp->query(new Where('g_id',1,'int'),"*",'GAttr');
+
+foreach ($gp as $g){
+    $gp->set('g_img',str_replace('_430x430q90.jpg','',$g['g_img']));
+}
+$gp->save();
 
 
